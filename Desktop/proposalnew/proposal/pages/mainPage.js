5405ec66726d7d1c9144ab10acbd5701b7cb2e76
KhangalEb <124862257+KhangalEb@users.noreply.github.com>
2023-05-31 15:06:44 +0800

    m

--- Desktop/proposalnew/proposal/pages/mainPage.js
+++ Desktop/proposalnew/proposal/pages/mainPage.js
@@ -42,42 +42,57 @@ const Home = () => {
 
 
     confetti({
-      particleCount: 150,
+      particleCount: 900,
       spread: 360
     });
-    setStateText("Баярлалаа хайртай шүү үнсий");
+    setStatePage(1);
+    setStateText("Хорвоогийн хамгийн swag залуутай үерхсэнд тань баяр хүргэе! 😘");
   }
 
   const [stateText, setStateText] = useState("Надтай үерхээч?");
+  const [statePage, setStatePage] = useState(0)
   const handleClick = (() => {
     state === 0 ? setState(60) : setState(0);
-    setStateText("За яахав нэг удаадаа өршөөе :)")
+    setStateText("За яахав энэ удаадаа өршөөе :)")
   }
   )
 
+  if (statePage === 1) {
+    return (
+      <div className="flex justify-center items-center h-screen w-full bg-blue-400">
+        <div className=" bg-white rounded shadow-2xl p-8 m-4 text-black h-64 w-96">
+          <h1 className=" text-center font-mono font-extrabold my-4">{stateText}</h1>
+        </div>
 
-  return (
-    <div className="flex justify-center items-center h-screen w-full bg-blue-400">
-      <div className=" bg-white rounded shadow-2xl p-8 m-4 text-black h-64 w-96">
-        <h1>{stateText}</h1>
-        <button
-          className=" w-1/4 bg-teal-400 hover:bg-teal-600 text-white uppercase text-lg  p-2 rounded"
-          type="submit"
-          onClick={() => handleYes()}
-        >
-          Yes
-        </button>
-        <button
-          className={"w-1/4 bg-teal-400 hover:bg-teal-600 text-white uppercase text-lg p-2 rounded " + (state === 60 ? 'ml-40' : 'ml-0')}
-          type="submit"
-          onClick={() => handleClick()}
-        >
-          No
-        </button>
-      </div>
+      </div >
+    )
+  } else {
+    return (
+      <div className="flex justify-center items-center h-screen w-full bg-blue-400">
+        <div className=" bg-white rounded shadow-2xl p-8 m-4 text-black h-64 w-96">
+          <h1 className=" text-center font-mono font-extrabold my-4">{stateText}</h1>
+          <div className="">
+            <button
+              className=" w-1/4  bg-teal-400 hover:bg-teal-600 text-white uppercase text-lg  p-2 rounded"
+              type="submit"
+              onClick={() => handleYes()}
+            >
+              <p className=" text-center font-mono font-extrabold ">Тэгье</p>
+            </button>
+            <button
+              className={"w-1/4 bg-red-400  text-white uppercase text-lg p-2 rounded  " + (state === 60 ? 'ml-60' : 'ml-4')}
+              type="submit"
+              onClick={() => handleClick()}
+            >
+              <p className=" text-center font-mono font-extrabold ">Үгүй</p>
+            </button>
+          </div>
+        </div>
 
-    </div >
-  );
+      </div >
+    )
+  }
+
 }
 
 export default Home;
